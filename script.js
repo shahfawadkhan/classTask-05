81a5b5775cdefba65f58ebfd8df5eb5a6e64cdb8
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-28 23:36:47 -0700

    get current date and also store the date of input in variable

---
+++ script.js
@@ -0,0 +1,7 @@
+let yourAge = document.getElementById("myage");
+let calculateBtn = document.getElementById("calculate");
+
+let todayDate = new Date();
+let currentYear = todayDate.getFullYear();
+let currentMonth = todayDate.getMonth() + 1; 
+let currentDay = todayDate.getDate();
