4a1468637fa2352515b0b42e0b785d00c6e05ae3
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-28 23:41:19 -0700

    calculate years

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ let todayDate = new Date();
 let currentYear = todayDate.getFullYear();
 let currentMonth = todayDate.getMonth() + 1; 
 let currentDay = todayDate.getDate();
+
+
+calculateBtn.addEventListener('click', function() {
+    let birthYear = parseInt(yourAge.value);
+    let birthMonth = 0; 
+    let birthDay = 0;   
+    let age = currentYear - birthYear;
+    console.log(age);
+
+
+})
+    
+   
