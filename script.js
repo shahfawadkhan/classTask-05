30a6b249e8222163d4283a24e1fc0a2646057ee4
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-29 00:43:14 -0700

    fix month and days issue

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let yourAge = document.getElementById("myage");
+let yourAge = document.getElementById("yourAge");
 let calculateBtn = document.getElementById("calculate");
 
 let todayDate = new Date();
@@ -6,25 +6,35 @@ let currentYear = todayDate.getFullYear();
 let currentMonth = todayDate.getMonth() + 1; 
 let currentDay = todayDate.getDate();
 
-
 let yearBox = document.getElementById("year");
 let monthBox = document.getElementById("months");
 let daysBox = document.getElementById("days");
 
 calculateBtn.addEventListener('click', function() {
-    let birthYear = parseInt(yourAge.value);
-    let birthMonth = 0; 
-    let birthDay = 0;   
-    // console.log(age);
-        let ageYears = currentYear - birthYear;
-        let ageMonths = currentMonth - birthMonth;
-        let ageDays = currentDay - birthDay;
-
-    yearBox.textContent = {ageYears};
-    monthBox.textContent = ageDays;
-    daysBox.textContent = ageDays;
-
     
-})
+    let birthDate = new Date(yourAge.value);
+    let birthYear = birthDate.getFullYear();
+    let birthMonth = birthDate.getMonth() + 1; 
+    let birthDay = birthDate.getDate();   
+  
+    let ageYears = currentYear - birthYear;
+    let ageMonths = currentMonth - birthMonth;
+    let ageDays = currentDay - birthDay;
+
+    if (ageDays < 0) {
+        ageMonths--;
+        ageDays += new Date(currentYear, currentMonth - 1, 0).getDate(); 
+    }
+
+    if (ageMonths < 0) {
+        ageYears--;
+        ageMonths += 12;
+    }
+
+    yearBox.textContent = ageYears;
+    monthBox.textContent = ageMonths;
+    daysBox.textContent = ageDays;
+});
+
 
 
