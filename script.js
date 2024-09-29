415583243fb01c3905c3148b436c0697eb65f977
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-28 23:45:53 -0700

    get age days age months

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ let currentMonth = todayDate.getMonth() + 1;
 let currentDay = todayDate.getDate();
 
 
+let yearBox = document.getElementById("#year");
+let monthBox = document.getElementById("#months");
+let daysBox = document.getElementById("#days");
+
 calculateBtn.addEventListener('click', function() {
     let birthYear = parseInt(yourAge.value);
     let birthMonth = 0; 
     let birthDay = 0;   
-    let age = currentYear - birthYear;
     console.log(age);
+        let ageYears = currentYear - birthYear;
+        let ageMonths = currentMonth - birthMonth;
+        let ageDays = currentDay - birthDay;
 
+        console.log(ageMonths)
+        console.log(ageDays)
 
 })
-    
-   
+
+
