2e2869bb954b52e7ec504bab5e5ee42a99295dc9
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-28 23:58:13 -0700

    set the values of year months and days button

--- script.js
+++ script.js
@@ -7,22 +7,24 @@ let currentMonth = todayDate.getMonth() + 1;
 let currentDay = todayDate.getDate();
 
 
-let yearBox = document.getElementById("#year");
-let monthBox = document.getElementById("#months");
-let daysBox = document.getElementById("#days");
+let yearBox = document.getElementById("year");
+let monthBox = document.getElementById("months");
+let daysBox = document.getElementById("days");
 
 calculateBtn.addEventListener('click', function() {
     let birthYear = parseInt(yourAge.value);
     let birthMonth = 0; 
     let birthDay = 0;   
-    console.log(age);
+    // console.log(age);
         let ageYears = currentYear - birthYear;
         let ageMonths = currentMonth - birthMonth;
         let ageDays = currentDay - birthDay;
 
-        console.log(ageMonths)
-        console.log(ageDays)
+    yearBox.textContent = `${ageYears}<br>Years`;
+    monthBox.textContent = ageDays;
+    daysBox.textContent = ageDays;
 
+    
 })
 
 
